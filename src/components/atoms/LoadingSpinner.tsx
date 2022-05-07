e47ea428a871904ad8c83a8c6fda75cfb4fed27b
Alieldin Ayman <alieldinayman@gmail.com>
2022-05-07 12:00:42 +0200

    Implement a LoadingSpinner atom component to handle loading animations

---
+++ src/components/atoms/LoadingSpinner.tsx
@@ -0,0 +1,11 @@
+import '@/components/atoms/LoadingSpinner.scss';
+
+function LoadingSpinner() {
+    return (
+        <div className="spinner-container">
+            <div className="loading-spinner" />
+        </div>
+    );
+}
+
+export default LoadingSpinner;
